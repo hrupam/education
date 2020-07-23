ac564de2d3b961e41bb30e88308a1247440f0098
Rupam <hrupam55@gmail.com>
2020-07-23 21:39:59 +0530

    Navbar changes made

--- js/main.js
+++ js/main.js
@@ -4,9 +4,22 @@ logo = document.querySelector('#left-logo');
 navList = document.querySelector("#nav-list");
 rightSearch = document.querySelector("#right-search");
 
+
 burger.addEventListener('click', () => {
     navbar.classList.toggle('nav-resp');
+    navbar.classList.toggle('nav-trans');
     logo.classList.toggle('v-resp');
     navList.classList.toggle('v-resp');
     rightSearch.classList.toggle('v-resp');
+
+    if (burger.children[1].id && burger.children[2].id) {
+        burger.children[1].id = "";
+        burger.children[2].id = "";
+
+    }
+    else {
+        burger.children[1].id = "line-2";
+        burger.children[2].id = "line-3";
+    }
+
 })
